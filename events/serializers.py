80644f6086c8925fb071e430f9858f5e00a1d317
jimmy353 <jimelias213@gmail.com>
2026-02-12 21:34:09 +0400

    Fix cloudinary image urls permanently

--- events/serializers.py
+++ events/serializers.py
@@ -3,8 +3,8 @@ from .models import Event
 
 
 class EventListSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
     organizer_name = serializers.CharField(source="organizer.email", read_only=True)
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
@@ -21,15 +21,21 @@ class EventListSerializer(serializers.ModelSerializer):
         ]
 
     def get_image(self, obj):
-        request = self.context.get("request")
-        if obj.image and request:
-            url = request.build_absolute_uri(obj.image.url)
-            return url.replace("http://", "https://")
-        return None
+        if not obj.image:
+            return None
+
+        url = str(obj.image.url)
+
+        # force https always
+        if url.startswith("http://"):
+            url = url.replace("http://", "https://")
+
+        return url
 
 
 class OrganizerEventSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
+
     class Meta:
         model = Event
         fields = [
@@ -42,11 +48,15 @@ class OrganizerEventSerializer(serializers.ModelSerializer):
         ]
 
     def get_image(self, obj):
-        request = self.context.get("request")
-        if obj.image and request:
-            url = request.build_absolute_uri(obj.image.url)
-            return url.replace("http://", "https://")
-        return None
+        if not obj.image:
+            return None
+
+        url = str(obj.image.url)
+
+        if url.startswith("http://"):
+            url = url.replace("http://", "https://")
+
+        return url
 
 
 class EventCreateSerializer(serializers.ModelSerializer):
