eacdb7c716ff37f2fadc1ad3a371bd19d057596f
jimmy353 <jimelias213@gmail.com>
2026-02-13 13:34:02 +0400

    Fix cloudinary image urls

--- events/serializers.py
+++ events/serializers.py
@@ -2,8 +2,19 @@ from rest_framework import serializers
 from .models import Event
 
 
+def fix_cloudinary_url(url):
+    if not url:
+        return None
+
+    # always force https
+    url = url.replace("http://", "https://")
+
+    return url
+
+
 class EventListSerializer(serializers.ModelSerializer):
     organizer_name = serializers.CharField(source="organizer.email", read_only=True)
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
@@ -19,8 +30,15 @@ class EventListSerializer(serializers.ModelSerializer):
             "organizer_name",
         ]
 
+    def get_image(self, obj):
+        if not obj.image:
+            return None
+        return fix_cloudinary_url(obj.image.url)
+
 
 class OrganizerEventSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Event
         fields = [
@@ -31,6 +49,11 @@ class OrganizerEventSerializer(serializers.ModelSerializer):
             "end_date",
             "image",
         ]
+
+    def get_image(self, obj):
+        if not obj.image:
+            return None
+        return fix_cloudinary_url(obj.image.url)
 
 
 class EventCreateSerializer(serializers.ModelSerializer):
